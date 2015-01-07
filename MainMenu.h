e1143e24d5af49fd98adc202629431c87b43f6f9
Krisha Orge <rocalworks@gmail.com>
2015-01-07 17:23:13 +0800

    renaming to LoadingScreen and MainMenu
    bugs: needs two buttons on LoadingScreen; else, app crashes (see
    spritebuilder > loadingscreen)

--- MainMenu.h
+++ MainMenu.h
@@ -1,13 +1,11 @@
 //
-//  Gameplay.h
+//  MainMenu.h
 //  PhysicsTest
 //
 //  Created by Ray Venenoso on 12/10/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
-#import "CCNode.h"
-
-@interface Gameplay : CCNode
+@interface MainMenu : CCNode
 
 @end
